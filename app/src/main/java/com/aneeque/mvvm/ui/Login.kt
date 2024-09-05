f74a53c6d37c4c1e0065c10c89eb3d9736ae1214
AneequeMalik <aneeque.malik@gmail.com>
2024-09-05 20:52:12 +0500

    First commit

---
+++ app/src/main/java/com/aneeque/mvvm/ui/Login.kt
@@ -0,0 +1,16 @@
+package com.aneeque.mvvm.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aneeque.mvvm.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+    }
+}
